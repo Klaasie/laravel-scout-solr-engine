4df511e039bd29b5abf2ee47e50356b59a8c74f7
Klaas Poortinga <poortinga.k@gmail.com>
2021-09-11 13:41:00 +0200

    Update documentation

--- config/scout-solr.php
+++ config/scout-solr.php
@@ -3,22 +3,43 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | Solr Admin config
+    | Solr Admin ConfigSet
     |--------------------------------------------------------------------------
     |
     | Set a default ConfigSet used for the createIndex command.
-    | The ConfigSet is required for the command to work.
+    | Without a ConfigSet Solr won't be able to create indexes through the command.
     |
     |
     */
     'create' => [
         'config_set' => env('SOLR_CONFIG_SET', '_default'),
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Solr Admin unload
+    |--------------------------------------------------------------------------
+    |
+    | Settings used for the deleteIndex command.
+    |
+    |
+    */
     'unload' => [
         'delete_index' => env('SOLR_UNLOAD_DELETE_INDEX', false),
         'delete_data_dir' => env('SOLR_UNLOAD_DELETE_DATA_DIR', false),
         'delete_instance_dir' => env('SOLR_UNLOAD_DELETE_INSTANCE_DIR', false),
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Solr Select
+    |--------------------------------------------------------------------------
+    |
+    | Solr does not allow for unlimited results. By default, Solr limits queries to 10 results.
+    | When a limit is not provided through the builder instance this config is used instead.
+    |
+    |
+    */
     'select' => [
         'limit' => env('SOLR_SELECT_DEFAULT_LIMIT', 10),
     ],
@@ -28,7 +49,8 @@ return [
     | Solr Endpoint
     |--------------------------------------------------------------------------
     |
-    | @todo documentation
+    | Set the default Solr instance for the client to work with.
+    | A core is not necessary here, the client will set the right core based on the model searchableAs() value.
     |
     |
     */

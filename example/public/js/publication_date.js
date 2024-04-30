bcd1dd5eb8e60f4cc3178ba25af599fa41a4ff83
Klaas Poortinga <poortinga.k@gmail.com>
2024-04-30 20:59:22 +0200

    Update NPM

--- example/public/js/publication_date.js
+++ example/public/js/publication_date.js
@@ -1,1 +1,45 @@
-IMask(document.querySelector('input[name="publication_date"]'),{mask:Date,pattern:"Y-m-d",blocks:{d:{mask:IMask.MaskedRange,from:1,to:31,maxLength:2},m:{mask:IMask.MaskedRange,from:1,to:12,maxLength:2},Y:{mask:IMask.MaskedRange,from:1e3,to:9999}},format:function(a){var e=a.getDate(),t=a.getMonth()+1;return e<10&&(e="0"+e),t<10&&(t="0"+t),[a.getFullYear(),t,e].join("-")},parse:function(a){var e=a.split("-");return new Date(e[0],e[1]-1,e[2])},autofix:!0,lazy:!0,overwrite:!0});
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************************!*\
+  !*** ./resources/js/publication_date.js ***!
+  \******************************************/
+var dateMask = IMask(document.querySelector('input[name="publication_date"]'), {
+  mask: Date,
+  pattern: 'Y-m-d',
+  blocks: {
+    d: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 31,
+      maxLength: 2
+    },
+    m: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+      maxLength: 2
+    },
+    Y: {
+      mask: IMask.MaskedRange,
+      from: 1000,
+      to: 9999
+    }
+  },
+  format: function format(date) {
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    if (day < 10) day = "0" + day;
+    if (month < 10) month = "0" + month;
+    return [year, month, day].join('-');
+  },
+  parse: function parse(str) {
+    var yearMonthDay = str.split('-');
+    return new Date(yearMonthDay[0], yearMonthDay[1] - 1, yearMonthDay[2]);
+  },
+  autofix: true,
+  lazy: true,
+  overwrite: true
+});
+/******/ })()
+;
